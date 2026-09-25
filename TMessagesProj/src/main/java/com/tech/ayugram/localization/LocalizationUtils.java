e85d39f5ad253daaac15e0091c97048805acb046
Anselmnn <5016708+Anselmnn@users.noreply.github.com>
2026-09-25 22:42:20 +0500

    fix: LocalizationUtils.getLocalizationAsset now accepts Locale object

--- TMessagesProj/src/main/java/com/tech/ayugram/localization/LocalizationUtils.java
+++ TMessagesProj/src/main/java/com/tech/ayugram/localization/LocalizationUtils.java
@@ -2,9 +2,17 @@ package com.tech.ayugram.localization;
 
 import android.content.Context;
 import android.text.TextUtils;
+import java.util.Locale;
 
 public class LocalizationUtils {
     public static final String DEFAULT_LOCALIZATION = "localization.json";
+
+    public static String getLocalizationAsset(Locale locale) {
+        if (locale == null) {
+            return DEFAULT_LOCALIZATION;
+        }
+        return "localization_" + locale.getLanguage() + "_" + locale.getCountry() + ".json";
+    }
 
     public static String getLocalizationAsset(String locale) {
         if (TextUtils.isEmpty(locale)) {
