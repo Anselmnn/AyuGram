d49e228f8fb3281649e97900469db99486fd6b33
Anselmnn <5016708+Anselmnn@users.noreply.github.com>
2026-09-25 23:02:22 +0500

    fix: fix org.telegram references in App module

--- TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
+++ TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
@@ -1,6 +1,6 @@
 package org.telegram.messenger;
 
-import org.telegram.messenger.regular.BuildConfig;
+import com.tech.ayugram.messenger.regular.BuildConfig;
 
 public class ApplicationLoaderImpl extends ApplicationLoader {
     @Override
